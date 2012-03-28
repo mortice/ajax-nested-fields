229b47b702f8b2877c0ed1528f6aa891bf35cdda
Tom Stuart <tom@therye.org>
2012-03-28 14:44:58 +0100

    Allow optional link text

--- lib/ajax-nested-fields/helpers.rb
+++ lib/ajax-nested-fields/helpers.rb
@@ -4,14 +4,15 @@ module AjaxNestedFields
       f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this, '#{container}')")
     end
 
-    def link_to_new_child(f, association)
+    def link_to_new_child(f, association, text=nil)
+      link_text = text || "Add #{association}"
       object = f.object.class.reflect_on_association(association.to_sym).klass.new
 
       fields = f.fields_for(association, object, :child_index => "new_#{association.singularize}") do |builder|
         render("#{association.singularize}_fields", :f => builder)
       end
 
-      link_to_function "Add #{association}", raw("add_child_field('#{association}', 'new_#{association.singularize}', \"#{escape_javascript(fields)}\")")
+      link_to_function link_text, raw("add_child_field('#{association}', 'new_#{association.singularize}', \"#{escape_javascript(fields)}\")")
     end
   end
 end
